05b9d1a9e7fe7d9ae9a3d7af2bbc337b75dd2297
dat truong <thelegendaglx@gmail.com>
2019-08-13 22:16:07 +0700

    ok

--- index.js
+++ index.js
@@ -39,8 +39,10 @@ app.use(express.static('assets'));
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header("Access-Control-Allow-Headers", "*");
     next();
 });
 
